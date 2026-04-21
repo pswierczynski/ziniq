86408d18a1401e25900de042f6295a6550406303
pswierczynski <przemek.swierczynski@gmail.com>
2026-04-21 12:45:14 +0200

    Initial comit

---
+++ assets/js/main.js
@@ -0,0 +1,349 @@
+/**
+* Template Name: Mamba - v4.3.0
+* Template URL: https://bootstrapmade.com/mamba-one-page-bootstrap-template-free/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+function image1() {
+  document.getElementById("image-1").style.display = "block";
+  document.getElementById("image-2").style.display = "none";
+  document.getElementById("image-3").style.display = "none";
+  document.getElementById("image-4").style.display = "none";
+  document.getElementById("image-5").style.display = "none";
+  document.getElementById("image-6").style.display = "none";
+  document.getElementById("image-7").style.display = "none";
+  document.getElementById("image-8").style.display = "none";
+  document.getElementById("image-9").style.display = "none";
+}
+function image2() {
+  document.getElementById("image-1").style.display = "none";
+  document.getElementById("image-2").style.display = "block";
+  document.getElementById("image-3").style.display = "none";
+  document.getElementById("image-4").style.display = "none";
+  document.getElementById("image-5").style.display = "none";
+  document.getElementById("image-6").style.display = "none";
+  document.getElementById("image-7").style.display = "none";
+  document.getElementById("image-8").style.display = "none";
+  document.getElementById("image-9").style.display = "none";
+}
+function image3() {
+  document.getElementById("image-1").style.display = "none";
+  document.getElementById("image-2").style.display = "none";
+  document.getElementById("image-3").style.display = "block";
+  document.getElementById("image-4").style.display = "none";
+  document.getElementById("image-5").style.display = "none";
+  document.getElementById("image-6").style.display = "none";
+  document.getElementById("image-7").style.display = "none";
+  document.getElementById("image-8").style.display = "none";
+  document.getElementById("image-9").style.display = "none";
+}
+function image4() {
+  document.getElementById("image-1").style.display = "none";
+  document.getElementById("image-2").style.display = "none";
+  document.getElementById("image-3").style.display = "none";
+  document.getElementById("image-4").style.display = "block";
+  document.getElementById("image-5").style.display = "none";
+  document.getElementById("image-6").style.display = "none";
+  document.getElementById("image-7").style.display = "none";
+  document.getElementById("image-8").style.display = "none";
+  document.getElementById("image-9").style.display = "none";
+}
+function image5() {
+  document.getElementById("image-1").style.display = "none";
+  document.getElementById("image-2").style.display = "none";
+  document.getElementById("image-3").style.display = "none";
+  document.getElementById("image-4").style.display = "none";
+  document.getElementById("image-5").style.display = "block";
+  document.getElementById("image-6").style.display = "none";
+  document.getElementById("image-7").style.display = "none";
+  document.getElementById("image-8").style.display = "none";
+  document.getElementById("image-9").style.display = "none";
+}
+function image6() {
+  document.getElementById("image-1").style.display = "none";
+  document.getElementById("image-2").style.display = "none";
+  document.getElementById("image-3").style.display = "none";
+  document.getElementById("image-4").style.display = "none";
+  document.getElementById("image-5").style.display = "none";
+  document.getElementById("image-6").style.display = "block";
+  document.getElementById("image-7").style.display = "none";
+  document.getElementById("image-8").style.display = "none";
+  document.getElementById("image-9").style.display = "none";
+}
+function image7() {
+  document.getElementById("image-1").style.display = "none";
+  document.getElementById("image-2").style.display = "none";
+  document.getElementById("image-3").style.display = "none";
+  document.getElementById("image-4").style.display = "none";
+  document.getElementById("image-5").style.display = "none";
+  document.getElementById("image-6").style.display = "none";
+  document.getElementById("image-7").style.display = "block";
+  document.getElementById("image-8").style.display = "none";
+  document.getElementById("image-9").style.display = "none";
+}
+function image8() {
+  document.getElementById("image-1").style.display = "none";
+  document.getElementById("image-2").style.display = "none";
+  document.getElementById("image-3").style.display = "none";
+  document.getElementById("image-4").style.display = "none";
+  document.getElementById("image-5").style.display = "none";
+  document.getElementById("image-6").style.display = "none";
+  document.getElementById("image-7").style.display = "none";
+  document.getElementById("image-8").style.display = "block";
+  document.getElementById("image-9").style.display = "none";
+}
+function image9() {
+  document.getElementById("image-1").style.display = "none";
+  document.getElementById("image-2").style.display = "none";
+  document.getElementById("image-3").style.display = "none";
+  document.getElementById("image-4").style.display = "none";
+  document.getElementById("image-5").style.display = "none";
+  document.getElementById("image-6").style.display = "none";
+  document.getElementById("image-7").style.display = "none";
+  document.getElementById("image-8").style.display = "none";
+  document.getElementById("image-9").style.display = "block";
+}
+
+jQuery(document).ready(function($){
+
+  $('.search-form').keyup(function(){
+
+    if($(this).val()){
+      $('.dropdown-search').addClass("active");
+    }else{
+      $('.dropdown-search').removeClass("active");
+    }
+  });
+
+  $(".icons-grid").each(function() {
+    var $lis = $('.icon-box', this);
+    if ($lis.length > 5) {
+      $(this).append('<div class="icon-box li_more"><div class="icon"> <img src="assets/img/Group-620.svg" class="img-fluid" alt=""> </div> <p>Więcej</p> </div>')
+      $(this).append('<div class="icon-box li_off li_hide"><div class="icon"> <img src="assets/img/Group-620.svg" class="img-fluid" alt=""> </div> <p>Mniej</p> </div>')
+    }
+    if ($lis.length > 5) {
+      $lis.slice(5).addClass("li_off");
+      $(".li_more").on("click", function() {
+        $(this).siblings().addClass('li_show');
+        $(this).addClass('li_off');
+        $(this).removeClass('li_show');
+      });
+      $(".li_hide", this).on("click", function() {
+        $(this).siblings().removeClass('li_show');
+        $(this).removeClass('li_show');
+        $(this).addClass('li_off');
+        $(this).parent().find(".li_more").addClass('li_show');
+      });
+    }
+  });
+
+    console.log('js - ok ');
+
+});
+
+(function() {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  /**
+   * Easy on scroll event listener
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = select('#navbar .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let header = select('#header')
+    let offset = header.offsetHeight
+
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: 'smooth'
+    })
+  }
+
+  /**
+   * Header fixed top on scroll
+   */
+  let selectHeader = select('#header')
+  if (selectHeader) {
+    let headerOffset = selectHeader.offsetTop
+    let nextElement = selectHeader.nextElementSibling
+    const headerFixed = () => {
+      if ((headerOffset - window.scrollY) <= 0) {
+        selectHeader.classList.add('fixed-top')
+        nextElement.classList.add('scrolled-offset')
+      } else {
+        selectHeader.classList.remove('fixed-top')
+        nextElement.classList.remove('scrolled-offset')
+      }
+    }
+    window.addEventListener('load', headerFixed)
+    onscroll(document, headerFixed)
+  }
+
+  /**
+   * Hero carousel indicators
+   */
+  let heroCarouselIndicators = select("#hero-carousel-indicators")
+  let heroCarouselItems = select('#heroCarousel .carousel-item', true)
+
+  heroCarouselItems.forEach((item, index) => {
+    (index === 0) ?
+    heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "' class='active'></li>":
+      heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "'></li>"
+  });
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
+
+  /**
+   * Mobile nav toggle
+   */
+  on('click', '.mobile-nav-toggle', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
+
+  /**
+   * Mobile nav dropdowns activate
+   */
+  on('click', '.navbar .dropdown > a', function(e) {
+    if (select('#navbar').classList.contains('navbar-mobile')) {
+      e.preventDefault()
+      this.nextElementSibling.classList.toggle('dropdown-active')
+    }
+  }, true)
+
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on('click', '.scrollto', function(e) {
+    if (select(this.hash)) {
+      e.preventDefault()
+
+      let navbar = select('#navbar')
+      if (navbar.classList.contains('navbar-mobile')) {
+        navbar.classList.remove('navbar-mobile')
+        let navbarToggle = select('.mobile-nav-toggle')
+        navbarToggle.classList.toggle('bi-list')
+        navbarToggle.classList.toggle('bi-x')
+      }
+      scrollto(this.hash)
+    }
+  }, true)
+
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash)
+      }
+    }
+  });
+
+  /**
+   * Porfolio isotope and filter
+   */
+  window.addEventListener('load', () => {
+    let portfolioContainer = select('.portfolio-container');
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+      });
+
+      let portfolioFilters = select('#portfolio-flters li', true);
+
+      on('click', '#portfolio-flters li', function(e) {
+        e.preventDefault();
+        portfolioFilters.forEach(function(el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+
+        portfolioIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+        portfolioIsotope.on('arrangeComplete', function() {
+          AOS.refresh()
+        });
+      }, true);
+    }
+
+  });
+
+  /**
+   * Initiate portfolio lightbox
+   */
+  const portfolioLightbox = GLightbox({
+    selector: '.portfolio-lightbox'
+  });
+
+
+})();
